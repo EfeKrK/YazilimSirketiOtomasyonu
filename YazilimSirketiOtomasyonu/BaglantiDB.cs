6ae2b3ece4bec1b2bcead0c0564205722ba25a4b
Efe Kürkçü <efecik1818@hotmail.com>
2024-02-17 13:52:21 +0300

    veritabanı baglantı classı oluşturuldu

---
+++ YazilimSirketiOtomasyonu/BaglantiDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace YazilimSirketiOtomasyonu
+{
+    internal class BaglantiDB
+    {
+        private string ConnectionString = "Data Source=EFEKRK;Initial Catalog=MarketYonetimSistemi;Integrated Security=True";
+        private SqlConnection connection;
+
+        public BaglantiDB()
+        {
+            connection = new SqlConnection(ConnectionString);
+        }
+
+        public SqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        public void BaglantiOpen()
+        {
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                    Console.WriteLine("Veritabanı bağlantısı açıldı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Veritabanı bağlantısı açılırken hata oluştu: " + ex.Message);
+            }
+        }
+
+        public void BaglantiClose()
+        {
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Closed)
+                {
+                    connection.Close();
+                    Console.WriteLine("Veritabanı bağlantısı kapatıldı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Veritabanı bağlantısı kapatılırken hata oluştu: " + ex.Message);
+            }
+
+        }
+    }
+}
--- YazilimSirketiOtomasyonu/Form1.cs
+++ YazilimSirketiOtomasyonu/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,18 @@ namespace YazilimSirketiOtomasyonu
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            BaglantiDB baglantiDB = new BaglantiDB();
+            baglantiDB.BaglantiOpen();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
